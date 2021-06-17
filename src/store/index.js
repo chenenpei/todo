623a702bd214ef8a0207038619b2de075556fc77
enpei.chen <chenenpei93@gmail.com>
2021-06-18 00:38:48 +0800

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import * as Vuex from "vuex";
+
+const store = Vuex.createStore({
+    state() {
+        return {
+            list: []
+        }
+    },
+    mutations: {}
+});
+
+export default store;
---
+++ src/views/Archive.vue
@@ -0,0 +1,10 @@
+<template>
+    <n-h1>Archive List</n-h1>
+</template>
+
+<script setup>
+import { NH1 } from "naive-ui";
+</script>
+
+<style>
+</style>
